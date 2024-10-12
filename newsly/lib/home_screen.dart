69ca13d156850096d8fb36ca6b647e90df02c6a9
Jorge M <38768001+jmayta1984@users.noreply.github.com>
2024-10-12 08:45:34 -0400

    Add files via upload

---
+++ newsly/lib/home_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = const [ Icon(Icons.search),  Icon(Icons.favorite)];
+
+  void _onTapPressed(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Newsly'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite), label: 'Favorites'),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onTapPressed,
+      ),
+      body: _pages[_selectedIndex],
+    );
+  }
+}
