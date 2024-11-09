4dfb99adba494f288b19a3fef66af8ee9e1421df
jmayta1984 <jmayta@pucp.edu.pe>
2024-11-09 08:52:29 -0500

    feat (auth): update login page

--- movie_app/lib/features/auth/presentation/login_page.dart
+++ movie_app/lib/features/auth/presentation/login_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_app/features/auth/presentation/blocs/login_bloc.dart';
+import 'package:movie_app/features/auth/presentation/blocs/login_event.dart';
+import 'package:movie_app/features/auth/presentation/blocs/login_state.dart';
+import 'package:movie_app/features/movies/presentation/pages/movies_page.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -18,47 +23,71 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                controller: _usernameController,
-                decoration: const InputDecoration(
-                  prefixIcon: Icon(Icons.person),
-                  border: OutlineInputBorder(),
-                  label: Text('Username'),
+        child: BlocListener<LoginBloc, LoginState>(
+          listener: (context, state) {
+            if (state is LoginSuccess) {
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const MoviesPage(),
+                  ));
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  content: Text(
+                      'Welcome: ${state.user.firstName} ${state.user.lastName}')));
+            } else if (state is LoginError) {
+              ScaffoldMessenger.of(context)
+                  .showSnackBar(SnackBar(content: Text(state.message)));
+            }
+          },
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: _usernameController,
+                  decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.person),
+                    border: OutlineInputBorder(),
+                    label: Text('Username'),
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                obscureText: _isPasswordHidden,
-                controller: _passwordController,
-                decoration: InputDecoration(
-                    prefixIcon: const Icon(Icons.lock),
-                    border: const OutlineInputBorder(),
-                    label: const Text('Password'),
-                    suffixIcon: IconButton(
-                        onPressed: () {
-                          setState(() {
-                            _isPasswordHidden = !_isPasswordHidden;
-                          });
-                        },
-                        icon: Icon(_isPasswordHidden
-                            ? Icons.visibility
-                            : Icons.visibility_off))),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  obscureText: _isPasswordHidden,
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                      prefixIcon: const Icon(Icons.lock),
+                      border: const OutlineInputBorder(),
+                      label: const Text('Password'),
+                      suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              _isPasswordHidden = !_isPasswordHidden;
+                            });
+                          },
+                          icon: Icon(_isPasswordHidden
+                              ? Icons.visibility
+                              : Icons.visibility_off))),
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SizedBox(
-                width: double.infinity,
-                child: OutlinedButton(onPressed: (){}, child: const Text('Login'))),
-            )
-          ],
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                    width: double.infinity,
+                    child: OutlinedButton(
+                        onPressed: () {
+                          final String username = _usernameController.text;
+                          final String password = _passwordController.text;
+                          context.read<LoginBloc>().add(LoginSubmitted(
+                              username: username, password: password));
+                        },
+                        child: const Text('Login'))),
+              )
+            ],
+          ),
         ),
       ),
     );
--- movie_app/lib/main.dart
+++ movie_app/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_app/features/auth/presentation/blocs/login_bloc.dart';
 import 'package:movie_app/features/auth/presentation/login_page.dart';
 
 void main() {
@@ -10,10 +12,13 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      darkTheme: ThemeData.dark(),
-      theme: ThemeData.light(),
-      home: const LoginPage(),
+    return BlocProvider<LoginBloc>(
+      create: (context) => LoginBloc(),
+      child: MaterialApp(
+        darkTheme: ThemeData.dark(),
+        theme: ThemeData.light(),
+        home: const LoginPage(),
+      ),
     );
   }
 }
